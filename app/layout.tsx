4b330e639672e4a77111e8d5a74d99f1b74cafe1
GRX199 <grx199@gmail.com>
2025-10-30 13:23:41 +0800

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Desa Tounelet 1 - Website Resmi",
+  title: "Desa Sejahtera - Website Resmi",
   description: "Website resmi Desa Sejahtera - Informasi, berita, dan layanan desa",
   generator: "v0.app",
 }
